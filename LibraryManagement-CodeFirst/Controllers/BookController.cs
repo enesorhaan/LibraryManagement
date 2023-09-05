8182a464d1c7e9a330f6ea12657c353c5afe1106
enesorhaan <enesorhan_1366@hotmail.com>
2023-09-06 02:16:02 +0300

    Add CRUD

---
+++ LibraryManagement-CodeFirst/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement_CodeFirst.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult BookList()
+        {
+            return View();
+        }
+    }
+}
---
+++ LibraryManagement-CodeFirst/Controllers/BookTypeController.cs
@@ -0,0 +1,59 @@
+﻿using LibraryManagement_CodeFirst.Context;
+using LibraryManagement_CodeFirst.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryManagement_CodeFirst.Controllers
+{
+    public class BookTypeController : Controller
+    {
+        MyDbContext _db;
+        public BookTypeController(MyDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult BookTypeList()
+        {
+            List<BookType> bookTypes = _db.BookTypes.ToList();
+            return View(bookTypes);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(BookType bookType)
+        {
+            _db.BookTypes.Add(bookType);
+            _db.SaveChanges();
+            return RedirectToAction("BookTypeList");
+        }
+
+        public IActionResult Edit(int id) 
+        {
+            BookType bookType = _db.BookTypes.Find(id);
+            return View(bookType); 
+        }
+
+        [HttpPost]
+        public IActionResult Edit(BookType bookType)
+        {
+            bookType.Status = Enums.DataStatus.Updated;
+            bookType.ModifiedDate = DateTime.Now;
+            _db.BookTypes.Update(bookType);
+            _db.SaveChanges();
+            return RedirectToAction("BookTypeList");
+        }
+
+        public IActionResult HardDelete (int id)
+        {
+            BookType bookType = _db.BookTypes.Find(id);
+            _db.BookTypes.Remove(bookType);
+            _db.SaveChanges();
+            return RedirectToAction("BookTypeList");
+        }
+    }
+}
